1d5ea7ce82833c326c9f9f650c018431f74a03f7
Siao, Yu-Jie <a6194315535@gmail.com>
2019-12-24 14:57:59 +0800

    fix cpp11 compile argument

--- lang.h
+++ lang.h
@@ -1,5 +1,5 @@
 char *c_compile_argv[] = {"gcc", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c11", "main.c", "-lm", "-o", "main", 0};
-char *cpp_compile_argv[] = {"g++", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c++11", "main.c", "-lm", "-o", "main", 0};
+char *cpp_compile_argv[] = {"g++", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c++11", "main.cpp", "-lm", "-o", "main", 0};
 
 char *c_execution[] = {"./main", 0};
 char *cpp_execution[] = {"./main", 0};
