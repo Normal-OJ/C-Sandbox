5b60b07cf4ead1a650abb683ebfe8f289e4bbad9
Siao, Yu-Jie <a6194315535@gmail.com>
2020-03-15 18:12:56 +0800

    fix c to c11 std

--- lang.h
+++ lang.h
@@ -1,4 +1,4 @@
-char *c_compile_argv[] = {"gcc", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c18", "main.c", "-lm", "-o", "main", 0};
+char *c_compile_argv[] = {"gcc", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c11", "main.c", "-lm", "-o", "main", 0};
 char *cpp_compile_argv[] = {"g++", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c++17", "main.cpp", "-lm", "-o", "main", 0};
 
 char *c_execution[] = {"./main", 0};
