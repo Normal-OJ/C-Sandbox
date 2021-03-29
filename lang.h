408d526d754df0436de59bc7a1b5c3939cfe69ac
bogay <pojay11523@gmail.com>
2021-03-30 00:44:34 +0800

    Compile every .c file

--- lang.h
+++ lang.h
@@ -1,4 +1,5 @@
-char *c_compile_argv[] = {"gcc", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c11", "real_main.c", "main.c", "-lm", "-o", "main", 0};
+// char *c_compile_argv[] = {"gcc", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c11", "real_main.c", "main.c", "-lm", "-o", "main", 0};
+char *c_compile_argv[] = {"sh", "-c", "gcc -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c11 *.c -lm -o main", 0};
 char *cpp_compile_argv[] = {"g++", "-DONLINE_JUDGE", "-O2", "-w", "-fmax-errors=3", "-std=c++17", "main.cpp", "-lm", "-o", "main", 0};
 
 char *c_execution[] = {"./main", 0};
